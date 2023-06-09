cfd8e440ecfc42fcb161ac933fadf70c292f3eaf
oxpas <oxpase@gmail.com>
2023-06-08 21:24:49 -0400

    Remove wing tip  wing skin  made the bone a cube  disabled animation  added texture for glass  working fine so far the be texure is messed up

--- src/main/java/com/example/RenderWings.java
+++ src/main/java/com/example/RenderWings.java
@@ -19,7 +19,7 @@ public class RenderWings extends ModelBase
     private WingSettings settings; // An instance of the WingSettings class, used to handle the settings for the wings.
     private ResourceLocation location; // The location of the texture for the wings.
     private ModelRenderer wing; // The model for the wing.
-    private ModelRenderer wingTip; // The model for the tip of the wing.
+//    private ModelRenderer wingTip; // The model for the tip of the wing.
     private boolean playerUsesFullHeight; // Flag to indicate if the player model uses its full height.
 
     // The constructor of the class, which is called when an instance of the class is created.
@@ -27,34 +27,34 @@ public class RenderWings extends ModelBase
     {
         this.mc = Minecraft.getMinecraft(); // Get the current Minecraft instance.
         this.settings = settings; // Set the WingSettings instance.
-        this.location = new ResourceLocation("wingsmod", "wings.png"); // Set the ResourceLocation instance with the path to the wings texture.
+        this.location = new ResourceLocation("wingsmod", "glass.png"); // Set the ResourceLocation instance with the path to the wings texture.
         this.playerUsesFullHeight = Loader.isModLoaded("animations"); // Check if the "animations" mod is loaded to set playerUsesFullHeight.
 
         // Setting texture offsets.
         setTextureOffset("wing.bone", 0, 0); // Set texture offset for wing bone.
-        setTextureOffset("wing.skin", -10, 8); // Set texture offset for wing skin.
-        setTextureOffset("wingtip.bone", 0, 5); // Set texture offset for wingtip bone.
-        setTextureOffset("wingtip.skin", -10, 18); // Set texture offset for wingtip skin.
+//        setTextureOffset("wing.skin", -10, 8); // Set texture offset for wing skin.
+//        setTextureOffset("wingtip.bone", 0, 5); // Set texture offset for wingtip bone.
+//        setTextureOffset("wingtip.skin", -10, 18); // Set texture offset for wingtip skin.
 
         // Creating wing model renderer.
         wing = new ModelRenderer(this, "wing"); // Create a new ModelRenderer instance for the wing.
-        wing.setTextureSize(30, 30); // Set the size of the texture for the wing.
-        wing.setRotationPoint(-2F, 0, 0); // Set the rotation point for the wing.
-        wing.addBox("bone", -10.0F, -1.0F, -1.0F, 10, 2, 2); // Add a box to the wing model to represent the bone.
-        wing.addBox("skin", -10.0F, 0.0F, 0.5F, 10, 0, 10); // Add a box to the wing model to represent the skin.
+        wing.setTextureSize(16, 16); // Set the size of the texture for the wing.
+        wing.setRotationPoint(0F, 0, 0); // Set the rotation point for the wing.
+        wing.addBox("bone", 0F, 0F, 0F, 10, 10, 10); // Add a box to the wing model to represent the bone.
+//        wing.addBox("skin", -10.0F, 0.0F, 0.5F, 10, 0, 10); // Add a box to the wing model to represent the skin.
 
         // Creating wing tip model renderer.
-        wingTip = new ModelRenderer(this, "wingtip"); // Create a new ModelRenderer instance for the wing tip.
-        wingTip.setTextureSize(30, 30); // Set the size of the texture for the wing tip.
-        wingTip.setRotationPoint(-10.0F, 0.0F, 0.0F); // Set the rotation point for the wing tip.
-        wingTip.addBox("bone", -10.0F, -0.5F, -0.5F, 10, 1, 1); // Add a box to the wing tip model to represent the bone.
-        wingTip.addBox("skin", -10.0F, 0.0F, 0.5F, 10, 0, 10); // Add a box to the wing tip model to represent the skin.
-        wing.addChild(wingTip); // Make the wingtip a child of the wing so that it rotates around the wing.
+//        wingTip = new ModelRenderer(this, "wingtip"); // Create a new ModelRenderer instance for the wing tip.
+//        wingTip.setTextureSize(30, 30); // Set the size of the texture for the wing tip.
+//        wingTip.setRotationPoint(-10.0F, 0.0F, 0.0F); // Set the rotation point for the wing tip.
+//        wingTip.addBox("bone", -10.0F, -0.5F, -0.5F, 10, 1, 1); // Add a box to the wing tip model to represent the bone.
+//        wingTip.addBox("skin", -10.0F, 0.0F, 0.5F, 10, 0, 10); // Add a box to the wing tip model to represent the skin.
+//        wing.addChild(wingTip); // Make the wingtip a child of the wing so that it rotates around the wing.
     }
 
     // The onRenderPlayer method, which is triggered after the player has been rendered.
     @SubscribeEvent
-    public void onRenderPlayer(RenderPlayerEvent.Post event)
+    public void onRenderPlayer(RenderPlayerEvent.Post event) //This line declares a method named onRenderPlayer with a parameter of type RenderPlayerEvent.Post named event
     {
         EntityPlayer player = event.entityPlayer; // Get the player entity from the event.
 
@@ -90,16 +90,16 @@ public class RenderWings extends ModelBase
         mc.getTextureManager().bindTexture(location); // Bind the texture for the wings.
 
         // Loop for rendering both wings.
-        for (int j = 0; j < 2; ++j)
+        for (int j = 0; j < 1; ++j)
         {
             // Enable culling, which decides which faces of the model are visible based on their orientation.
             GL11.glEnable(GL11.GL_CULL_FACE);
-            float f11 = (System.currentTimeMillis() % 1000) / 1000F * (float) Math.PI * 2.0F; // Calculate a repeating time-based value.
+//            float f11 = (System.currentTimeMillis() % 1000) / 1000F * (float) Math.PI * 2.0F; // Calculate a repeating time-based value.
             // Calculate the rotation angles for the wing and the wing tip based on the time-based value.
-            this.wing.rotateAngleX = (float) Math.toRadians(-80F) - (float) Math.cos((double)f11) * 0.2F;
-            this.wing.rotateAngleY = (float) Math.toRadians(20F) + (float) Math.sin(f11) * 0.4F;
-            this.wing.rotateAngleZ = (float) Math.toRadians(20F);
-            this.wingTip.rotateAngleZ = -((float)(Math.sin((double)(f11 + 2.0F)) + 0.5D)) * 0.75F;
+//            this.wing.rotateAngleX = (float) Math.toRadians(-80F) - (float) Math.cos((double)f11) * 0.2F;
+//            this.wing.rotateAngleY = (float) Math.toRadians(20F) + (float) Math.sin(f11) * 0.4F;
+            this.wing.rotateAngleZ = (float) Math.toRadians(0F);
+//            this.wingTip.rotateAngleZ = -((float)(Math.sin((double)(f11 + 2.0F)) + 0.5D)) * 0.75F;
             // Render the wing.
             this.wing.render(0.0625F);
             // Scale the model for the next wing.
